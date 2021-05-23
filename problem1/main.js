37231a7fb3caf0d3643b13f256b7dbec046e47d0
tstmia <tstmia0625@gmail.com>
2021-05-23 14:07:04 +0900

    fix format

--- problem1/main.js
+++ problem1/main.js
@@ -3,17 +3,16 @@
  * 同じようにして, 1000 未満の 3 か 5 の倍数になっている数字の合計を求めよ.
  */
 const main = (number) => {
-    let num = 1;
-    let sum = 0;
-    while(num < number){
-        if(num % 3 === 0 || num % 5 === 0){
-            sum += num;
-        }
-        num++;
+  let num = 1;
+  let sum = 0;
+  while (num < number) {
+    if (num % 3 === 0 || num % 5 === 0) {
+      sum += num;
     }
-    return sum;
-
-}
+    num++;
+  }
+  return sum;
+};
 
 const target = 1000;
-console.log('答え：', main(target));
+console.log("答え：", main(target));
